a74d90f85c658414ef4623c22c3758ac27dfb194
ATMITGIT <atmprogramming@gmail.com>
2020-05-19 20:38:32 +0400

    v1

---
+++ WebApplication21/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication21.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication21.Controllers
+{
+	public class UserController : Controller
+	{
+        private ApplicationContext db;
+        public UserController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string Name, string Email, string Password)
+        {
+           
+                User s = new User { Name = Name, Email = Email, Password = Password };
+                db.Add(s);
+                db.SaveChanges();
+                return RedirectPermanent("~/User/UserPage");
+            
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+
+
+        public IActionResult Login(string Email, string Password)
+        {
+            bool t = db.Users.Any(x => x.Email == Email && x.Password == Password);
+           
+                return RedirectPermanent("~/User/UserPage");
+           
+
+        }
+        public IActionResult UserPage()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ WebApplication21/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication21.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		//public int PeopleId { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
